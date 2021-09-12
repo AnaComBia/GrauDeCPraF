fa395fdd77b67f5617adade8854422d0e1f7e244
AnaComBia <anabeatrizetorres@gmail.com>
2021-09-12 13:51:15 -0300

    finaliza programa Conversor de Celsius pra fahrenheit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GrauDeCPraF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n---olá usuário, aqui irei fazer um conversor de temperatura de Celsius para fahrenheit----\n");
+
+            Console.Write("Temperatura em Celsius: ");
+            double TemperaturaC = Convert.ToDouble(Console.ReadLine());
+
+            double TemperaturaF = TemperaturaC * 1.8 + 32; 
+
+            Console.WriteLine($"\nTemperatura em fahrenheit: {TemperaturaF}\n"); 
+
+
+        }
+    }
+}
